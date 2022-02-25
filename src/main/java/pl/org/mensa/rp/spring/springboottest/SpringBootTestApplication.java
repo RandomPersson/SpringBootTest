4961defc18cba0a0263035c312a70de0152a5274
Random Persson <random.rp.persson@gmail.com>
2022-02-25 07:50:59 +0100

    Prepared for Tomcat deployment (tested)

--- src/main/java/pl/org/mensa/rp/spring/springboottest/SpringBootTestApplication.java
+++ src/main/java/pl/org/mensa/rp/spring/springboottest/SpringBootTestApplication.java
@@ -3,12 +3,13 @@ package pl.org.mensa.rp.spring.springboottest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
-
+public class SpringBootTestApplication extends SpringBootServletInitializer {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
 	}
